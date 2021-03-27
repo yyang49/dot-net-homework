03eb8ff8870dcd1879997e5b010aff14f01963d8
yyang49 <yyang@yyanglin.com>
2021-03-27 23:34:47 +0800

    submit homework4

---
+++ Homework4/Alarm/Program.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace Alarm
+{
+    public class AlarmEventArgs
+    {
+        public int Hour { get; set; }
+        public int Minute { get; set; }
+        public int Second { get; set; }
+    }
+    public class TickEventArgs
+    {
+        public int Hour { get; set; }
+        public int Minute { get; set; }
+        public int Second { get; set; }
+    }
+    class Clock
+    {
+        public event Action<object, AlarmEventArgs> AlarmEvent;
+        public event Action<object, TickEventArgs> TickEvent;
+        private int alarmHour;
+        private int alarmMinute;
+        private int alarmSecond;
+        public void SetAlarm(int alarmHour, int alarmMinute, int alarmSecond)
+        {
+            this.alarmHour = alarmHour;
+            this.alarmMinute = alarmMinute;
+            this.alarmSecond = alarmSecond;
+        }
+        public void Start()
+        {
+            while (true)
+            {
+                int hour = DateTime.Now.Hour;
+                int minute = DateTime.Now.Minute;
+                int second = DateTime.Now.Second;
+                Tick(hour, minute, second);
+                if (hour == this.alarmHour && minute == this.alarmMinute && second == this.alarmSecond)
+                {
+                    Alarm(hour, minute, second);
+                }
+                Thread.Sleep(1000);
+            }
+        }
+        public void Alarm(int hour, int minute, int second)
+        {
+            AlarmEventArgs args = new AlarmEventArgs() { Hour = hour, Minute = minute, Second = second };
+            AlarmEvent(this, args);
+        }
+        public void Tick(int hour, int minute, int second)
+        {
+            TickEventArgs args = new TickEventArgs() { Hour = hour, Minute = minute, Second = second };
+            TickEvent(this, args);
+        }
+    }
+    class Alarm
+    {
+        public Clock clock = new Clock();
+        public Alarm()
+        {
+            clock.AlarmEvent += ClockAlarm;
+            clock.TickEvent += ClockTick;
+        }
+        void ClockAlarm(object sender, AlarmEventArgs args)
+        {
+            Console.WriteLine($"Alarm {args.Hour}:{args.Minute}:{args.Second}");
+        }
+        void ClockTick(object sender, TickEventArgs args)
+        {
+            Console.WriteLine($"{args.Hour}:{args.Minute}:{args.Second}");
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Alarm alarm = new Alarm();
+            alarm.clock.SetAlarm(12, 0, 0);
+            alarm.clock.Start();
+        }
+    }
+}
---
+++ Homework4/GenericList/Program.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericList
+{
+    public class Node<T>
+    {
+        public Node<T> Next { get; set; }
+        public T Data { get; set; }
+
+        public Node(T t)
+        {
+            Next = null;
+            Data = t;
+        }
+    }
+
+    public class GenericList<T>
+    {
+        private Node<T> head;
+        private Node<T> tail;
+
+        public GenericList()
+        {
+            tail = head = null;
+        }
+
+        public Node<T> Head
+        {
+            get => head;
+        }
+
+        public void Add(T t)
+        {
+            Node<T> n = new Node<T>(t);
+            if (tail == null)
+            {
+                head = tail = n;
+            }
+            else
+            {
+                tail.Next = n;
+                tail = n;
+            }
+        }
+
+        public void ForEach(Action<T> action)
+        {
+            for (Node<T> node = head; node != null; node = node.Next)
+            {
+                action(node.Data);
+            }
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            GenericList<int> list = new GenericList<int>();
+            list.Add(1);list.Add(2);list.Add(3);list.Add(4);list.Add(5);
+            list.ForEach(s => Console.WriteLine(s));
+            int max = list.Head.Data;
+            list.ForEach(i => max = i > max ? i : max);
+            Console.WriteLine("最大值是" + max);
+            int min = list.Head.Data;
+            list.ForEach(i => min = min > i ? i : min);
+            Console.WriteLine("最小值是" + min);
+            int sum = 0;
+            list.ForEach(i => sum += i);
+            Console.WriteLine("和为" + sum);
+            Console.ReadLine();
+        }
+    }
+}
